bff66bfe706cceaf289d1805f78308c227c01d0b
haolink <5476865+haolink@users.noreply.github.com>
2018-09-11 19:58:42 +0200

    Switching to real time assembly generation.

---
+++ GG2PlayerScale/Assembly/patch.asm
@@ -0,0 +1,118 @@
+use64
+
+camscale:
+  ;General data
+  dd 3F800000h ;Scale
+  dd 00000000h ;Stored height
+  dd 00000000h
+  dd 00000000h
+  ;Base scale 1.0
+  dd 00000000h
+  dd 00000000h
+  dd 3F800000h
+  dd 00000000h
+  ;Value to subtract
+  dd 00000000h
+  dd 00000000h
+  dd 00000000h
+  dd 00000000h
+  ;Value to multiply it with
+  dd 00000000h
+  dd 00000000h
+  dd 00000000h
+  dd 00000000h
+  ;Live height adjust
+  dd 00000000h
+  dd 00000000h
+  dd 00000000h
+  dd 00000000h
+  ;Subtitle adjust
+  dd 00000000h
+  dd 00000000h
+  dd 41F00000h
+  dd 00000000h
+
+;Some distance so the code segments are nicely separated, makes debugging in CheatEngine easier.
+align 10h
+dd 00000000h  
+align 20h
+db 'CAMUPDATE' ;Identifier
+align 20h
+
+;This code segment is being executed whenever the player warps.
+;It fixates a scale and the game will adjust all overlays to the current height.
+camscale_assembly_codebegin:
+   movaps xmm1,[rcx+00000190h]
+
+   push rax
+   push rbx
+   mov rax,[rcx]
+   mov ebx,[rax-06D0h]
+   cmp ebx,$006F0052
+   jne camscale_skip
+   mov rbx,0xDEADCAFEDEADCAFE  ;Identifier
+   mov eax,[rbx]
+   mov [rbx+28h],eax
+   mov eax,[rcx+00000164h]
+   mov [rbx+04h],eax
+   mov [rbx+38h],eax
+   movaps xmm0,[rbx+10h]
+   subps xmm0,[rbx+20h]
+   mulps xmm0,[rbx+30h]
+   subps xmm1,xmm0
+camscale_skip:
+   pop rbx
+   pop rax
+
+   mov rax,rdx
+   movaps xmm0,xmm1
+   movss [rdx],xmm1
+   jmp near [rip]
+   dq 'RETURN01' ;Identifier
+
+;Some distance so the code segments are nicely separated, makes debugging in CheatEngine easier.
+align 10h
+dd 00000000h
+align 20h   
+db 'LIVECAM' ;Identifier
+align 20h
+
+;This code segment is being executed multiple times per second while the game isn't pause.
+;Here live scaling and subtitle adjustment will take place.
+camscale_live_assembly_codebegin:
+   movaps xmm1,[rcx+0190h]
+   push rax
+   push rbx
+   mov rbx,0xDEADCAFEDEADCAFE  ;Identifier
+   ;General checks that we're catching the camera and nothing else
+   mov eax,[rcx+01BCh]
+   cmp eax,00020011h
+   jne livecamscale_skip   
+   mov eax,[rcx+017Ch]
+   cmp eax,3F800000h   
+   jne livecamscale_skip
+   mov eax,[rcx+0018h]
+   cmp eax,00000065h
+   je livecamsubtitle_adjust ;This is the case when the UI overlay renderer was accidentially caught
+   mov eax,[rcx+0118h]
+   cmp eax,00000008h
+   jne livecamscale_skip
+   mov eax,[rcx+0100h]
+   cmp eax,00000000h
+   je livecamscale_skip
+   mov eax,[rcx+01A8h]	
+   cmp eax,3F800000h
+   jne livecamscale_skip
+
+livecamscale_adjust:
+   addps xmm1,[rbx+0040h]
+   jmp livecamscale_skip
+livecamsubtitle_adjust:
+   addps xmm1,[rbx+0050h]
+livecamscale_skip:
+   pop rbx
+   pop rax
+   movaps [rbx+0010h],xmm1
+   movaps xmm0,[rcx+01A0h]
+   jmp near [rip]
+   dq 'RETURN02' ;Identifier
---
+++ GG2PlayerScale/AssemblyPatchManager.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Resources;
+
+namespace GG2PlayerScale
+{
+    public class AssemblyPatchManager
+    {
+        /// <summary>
+        /// Assembly code.
+        /// </summary>
+        public byte[] AssemblyCode { get; private set; }
+
+        /// <summary>
+        /// Player scale to write.
+        /// </summary>
+        public int ScaleOffset { get; private set; }
+
+        /// <summary>
+        /// Current player scale.
+        /// </summary>
+        public int CurrentScaleOffset { get; private set; }
+
+        /// <summary>
+        /// Height of the current scene for the player.
+        /// </summary>
+        public int DefaultSceneHeightOffset { get; private set; }
+
+        /// <summary>
+        /// Camera offset to use.
+        /// </summary>
+        public int CurrentCameraOffset { get; private set; }
+
+        /// <summary>
+        /// Subtitle rendering offset.
+        /// </summary>
+        public int SubtitleOffset { get; private set; }
+
+        /// <summary>
+        /// The base address the patch code is added to needs to be stored here.
+        /// </summary>
+        public int[] BaseAddressOffsets { get; private set; }
+
+        /// <summary>
+        /// First code patch entry offset.
+        /// </summary>
+        public int Patch1EntryOffset { get; private set; }
+
+        /// <summary>
+        /// First code patch leaving offset.
+        /// </summary>
+        public int Patch1LeavingOffset { get; private set; }
+
+        /// <summary>
+        /// Second code patch entry offset.
+        /// </summary>
+        public int Patch2EntryOffset { get; private set; }
+
+        /// <summary>
+        /// Second code patch leaving offset.
+        /// </summary>
+        public int Patch2LeavingOffset { get; private set; }
+
+        public AssemblyPatchManager()
+        {
+            byte[] patchCode = GG2PlayerScale.Properties.Resources.patch;
+            this.AssemblyCode = patchCode;
+
+            this.ScaleOffset = 0x00;
+            this.CurrentScaleOffset = 0x28;
+            this.DefaultSceneHeightOffset = 0x04;
+            this.CurrentCameraOffset = 0x48;
+            this.SubtitleOffset = 0x58;
+
+            long[] offsets = patchCode.IndexesOf(Encoding.ASCII.GetBytes("RETURN01")).ToArray();
+            if(offsets.Length != 1)
+            {
+                throw new Exception("Unable to find RETURN01");
+            }
+            this.Patch1LeavingOffset = (int)offsets[0];
+
+            offsets = patchCode.IndexesOf(Encoding.ASCII.GetBytes("RETURN02")).ToArray();
+            if (offsets.Length != 1)
+            {
+                throw new Exception("Unable to find RETURN02");
+            }
+            this.Patch2LeavingOffset = (int)offsets[0];
+
+            offsets = patchCode.IndexesOf(BitConverter.GetBytes(0xDEADCAFEDEADCAFEL)).ToArray();
+            if (offsets.Length != 2)
+            {
+                throw new Exception("Unable to find DEADCAFE");
+            }
+            this.BaseAddressOffsets = new int[] { (int)offsets[0], (int)offsets[1] };
+
+            offsets = patchCode.IndexesOf(Encoding.ASCII.GetBytes("CAMUPDATE")).ToArray();
+            if (offsets.Length != 1)
+            {
+                throw new Exception("Unable to find CAMUPDATE");
+            }
+            this.Patch1EntryOffset = (int)(offsets[0] + 0x20);
+
+            offsets = patchCode.IndexesOf(Encoding.ASCII.GetBytes("LIVECAM")).ToArray();
+            if (offsets.Length != 1)
+            {
+                throw new Exception("Unable to find LIVECAM");
+            }
+            this.Patch2EntryOffset = (int)(offsets[0] + 0x20);
+        }
+    }
+}
